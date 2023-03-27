fbece900751188550eb1464582ea96eacda80ea3
Luis Gallo <luisgallo808@gmail.com>
2023-03-26 22:01:08 -0400

    11

--- deno-api/index.js
+++ deno-api/index.js
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
-  return new Response("Hello World2!", {
+  return new Response("Hello World3!", {
     headers: { "content-type": "text/plain" },
   });
 });
